267740494bc4ab42faecd20dc90e70fe9f878943
KrishanSunil <Krishantha.Sunil@fox.com>
2016-03-13 20:44:26 +0800

    Initial Commit

---
+++ playTennis/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  playTennis
+//
+//  Created by Krishantha Sunil Premaretna on 13/3/16.
+//  Copyright © 2016 Krishantha Sunil Premaretna. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ playTennis/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  playTennis
+//
+//  Created by Krishantha Sunil Premaretna on 13/3/16.
+//  Copyright © 2016 Krishantha Sunil Premaretna. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
